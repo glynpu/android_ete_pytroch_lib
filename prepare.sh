ef09aba0d36e0867052ab8776893ee6bf2faf321
glynpu <839019390@qq.com>
2021-01-03 23:14:25 +0800

    models for android demo

--- prepare.sh
+++ prepare.sh
@@ -1,3 +1,7 @@
 # scp lyguo@mobvoi-maryland:/export/maryland/lyguo/gerrit/speech/recognizer/feats_cmvn.ark ./
+/home/chaoyang/e2e/wenet/examples/aishell/s1/exp/torchaudio_baseline/final.zip
 scp lyguo@mobvoi-maryland:/export/maryland/binbinzhang/code/wenet/examples/aishell/s0//data/dict/lang_char.txt ./
+
+model_path:
+链接: https://pan.baidu.com/s/1BnT6KeAbsAPOTkIRp5Zwew 提取码: 7rci 复制这段内容后打开百度网盘手机App，操作更方便哦
 
